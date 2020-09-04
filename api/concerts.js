d9b1af6cf96c612748fee302bcfcb4c4ed9ceb87
Hans Henrik Grønsleth <hh.gronsleth@gmail.com>
2020-09-04 16:26:20 +0200

    Return a fixture if development mode
    To avoid hammering Songkicks API

--- api/concerts.js
+++ api/concerts.js
@@ -8,31 +8,37 @@ export default async (request, response) => {
   let morePagesAvailable = true;
   let currentPage = 0;
 
-  while (morePagesAvailable) {
-    currentPage++;
+  if (process.env.NODE_ENV == "development") {
+    console.log("NB! Returning fixtures since development mode");
+    const fixtures = require("./concerts-fixture.json");
+    response.status(200).send(fixtures);
+  } else {
+    while (morePagesAvailable) {
+      currentPage++;
 
-    const res = await fetch(
-      "https://api.songkick.com/api/3.0/metro_areas/" +
-        areaId +
-        "/calendar.json?" +
-        new URLSearchParams({
-          apikey: process.env.SONGKICK_API_KEY,
-          page: currentPage,
-        })
-    );
+      const res = await fetch(
+        "https://api.songkick.com/api/3.0/metro_areas/" +
+          areaId +
+          "/calendar.json?" +
+          new URLSearchParams({
+            apikey: process.env.SONGKICK_API_KEY,
+            page: currentPage,
+          })
+      );
 
-    const {
-      resultsPage: {
-        results: { event },
-        perPage,
-        totalEntries,
-      },
-    } = await res.json();
+      const {
+        resultsPage: {
+          results: { event: events },
+          perPage,
+          totalEntries,
+        },
+      } = await res.json();
 
-    const totalPages = Math.ceil(totalEntries / perPage);
-    event.forEach((e) => allData.unshift(e));
-    morePagesAvailable = currentPage < totalPages;
+      const totalPages = Math.ceil(totalEntries / perPage);
+      events.forEach((e) => allData.unshift(e));
+      morePagesAvailable = currentPage < totalPages;
+    }
+
+    response.status(200).send(allData);
   }
-
-  response.status(200).send(allData);
 };
